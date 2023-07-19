0397eaf29a0953dc68eb801ea2163991597cc15d
Semyon Golgan <94925577+black-evil-dragon@users.noreply.github.com>
2023-07-19 19:14:07 +0300

    create pre welcome component

--- induck/src/index.tsx
+++ induck/src/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes } from 'react-router-dom';
 
 import './styles/scss/index.scss';
 
 import App from './App';
+
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
 root.render(
 	<React.StrictMode>
-		<App />
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
 	</React.StrictMode>
 )
