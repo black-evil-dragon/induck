618de6dd02c68a789a7d85409845a4e1ba96f7c4
Semyon Golgan <blackevildragon@mail.ru>
2024-01-07 11:44:08 +0300

    test

--- induck/src/index.tsx
+++ induck/src/index.tsx
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
 	<React.StrictMode>
-		<BrowserRouter>
+		<BrowserRouter basename={'https://black-evil-dragon.github.io/induck'}>
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>
