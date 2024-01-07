25de572b05635b90cd4e5a91bf50f7d46229a9c2
Semyon Golgan <blackevildragon@mail.ru>
2024-01-07 13:15:57 +0300

    #

--- induck/src/index.tsx
+++ induck/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, HashRouter } from 'react-router-dom';
 
 import './styles/scss/index.scss';
 
@@ -13,12 +13,12 @@ const root = ReactDOM.createRoot(
 );
 root.render(
 	<React.StrictMode>
-		<BrowserRouter basename="/induck">
+		<HashRouter basename="/induck">
 			<Routes>
 				<Route path='/' element={<App />}/>
 				<Route path='/signup' element={<SignUp />}/>
 			</Routes>
-		</BrowserRouter>
+		</HashRouter>
 	</React.StrictMode>
 )
 
